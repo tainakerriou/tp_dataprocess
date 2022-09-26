4f9086afff3aae6e35006fcd73876a30d670cfa8
taina <taina.kerriou@epfedu.fr>
2022-09-26 09:59:27 +0200

    tp1

--- tp-kafka-api/src/main/scala/com/github/polomarcus/utils/KafkaAvroConsumerService.scala
+++ tp-kafka-api/src/main/scala/com/github/polomarcus/utils/KafkaAvroConsumerService.scala
@@ -27,7 +27,7 @@ object KafkaAvroConsumerService {
   props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, kafkaAvroDeserializer)
   props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, ConfService.SCHEMA_REGISTRY)
   props.put(ConsumerConfig.GROUP_ID_CONFIG, ConfService.GROUP_ID)
-  val autoReset = ??? //@TODO @see https://kafka.apache.org/documentation/#consumerconfigs_auto.offset.reset
+  val autoReset = "earliest" //@TODO @see https://kafka.apache.org/documentation/#consumerconfigs_auto.offset.reset
   props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoReset)
 
   private val consumer = new KafkaConsumer[String, Record](props)
@@ -56,8 +56,8 @@ object KafkaAvroConsumerService {
           messages.forEach(record => {
 
             //@TODO how can we parse the raw data to a News object? @see producer for hints
-            val deserializedValue = ???
-
+             val deserializedValue = RecordFormat[News].from(record.value())
+// Deserialized Value (Class): title ${deserializedValue.title } media ${deserializedValue.media }
             logger.info(
               s"""Consumed :
                  |Offset : ${record.offset()} from partition ${record.partition()}
